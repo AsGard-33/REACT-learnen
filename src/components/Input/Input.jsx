46b18030422b0c13ab67cfa187e87d6fe08ea582
Akuji <akujil26@gmail.com>
2024-02-19 11:56:05 +0100

    popravkahomework18

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,16 +1,15 @@
 import "./style.css";
+
 function Input({ name, type, placeholder, label }) {
   return (
     <div className="input">
       <label htmlFor={name}>{label}</label>
       <input
-        
         id={name}
         name={name}
         type={type}
         placeholder={placeholder}
         className="input-field"
-        required
       />
     </div>
   );
